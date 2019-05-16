c4bdc1f486cf05a8516d82a430582e02ff6c3499
Tim Halbach <tim.halbach@mt-ag.com>
2019-05-16 11:33:59 +0200

    edit shell script

--- Docker/scripts/Windows.sh
+++ Docker/scripts/Windows.sh
@@ -1,12 +1,14 @@
-docker run --detach --hostname gitlab.example.com --publish 443:443 --publish 80:80 --publish 22:22 --name gitlab --restart always --volume  \srv\gitlab\config:/etc/gitlab --volume \srv\gitlab\logs:/var/log/gitlab --volume \srv\gitlab\data:/var/opt/gitlab gitlab/gitlab-ce:11.9.7-ce.0
-
-
 docker run --detach `
   --hostname gitlab.example.com `
   --publish 443:443 --publish 80:80 --publish 22:22 `
   --name gitlab `
   --restart always `
-  --volume /Users/thalbach/Documents/Documents/Git/Docker/srv/gitlab/config:/etc/gitlab `
-  --volume /Users/thalbach/Documents/Documents/Git/Docker/srv/gitlab/logs:/var/log/gitlab `
-  --volume /Users/thalbach/Documents/Documents/Git/Docker/srv/gitlab/data:/var/opt/gitlab `
+  --volume <kommpletePfadAngabe>\Docker\srv\gitlab\config:/etc/gitlab `
+  --volume <kommpletePfadAngabe>\Docker\srv\gitlab\logs:/var/log/gitlab `
+  --volume <kommpletePfadAngabe>\Docker\srv\gitlab\data:/var/opt/gitlab `
   gitlab/gitlab-ce:11.9.7-ce.0
+
+# Beispiel --volume /Users/thalbach/Documents/Documents/Git/Docker/srv/gitlab/data:/var/opt/gitlab `
+
+# Beispiel in einer Zeile
+#docker run --detach --hostname gitlab.example.com --publish 443:443 --publish 80:80 --publish 22:22 --name gitlab --restart always --volume  \srv\gitlab\config:/etc/gitlab --volume \srv\gitlab\logs:/var/log/gitlab --volume \srv\gitlab\data:/var/opt/gitlab gitlab/gitlab-ce:11.9.7-ce.0
